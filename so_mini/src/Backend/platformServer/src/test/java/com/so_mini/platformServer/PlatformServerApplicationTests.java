65c2e1c1d754ee36faed24f030193d89a0b80a8d
Leejieun3001 <jieun3001@gmail.com>
2020-02-10 00:40:27 +0900

    init jieun project

---
+++ so_mini/src/Backend/platformServer/src/test/java/com/so_mini/platformServer/PlatformServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.so_mini.platformServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatformServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
