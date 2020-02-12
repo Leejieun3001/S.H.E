bbcd154d00421282f04d984cb693f1d05e9bd060
jihyuno301 <joh755807@gmail.com>
2020-02-12 16:27:06 +0900

    semantic UI Vue 적용

---
+++ so_mini/src/frontend/src/components/login.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+  <div>
+    <h3>SO_MINI</h3>
+    <sui-input placeholder="사용자이름 또는 이메일" />
+    <sui-input placeholder="비밀번호" />
+    <sui-button loading primary content="로그인" />
+  </div>
+  <div></div>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "login"
+    }
+</script>
+
+<style scoped>
+.container {
+  text-align : center;
+}
+</style>
--- so_mini/src/frontend/src/main.js
+++ so_mini/src/frontend/src/main.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
+import SuiVue from 'semantic-ui-vue';
 import App from './App.vue'
+
+Vue.use(SuiVue);
 
 new Vue({
   el: '#app',
