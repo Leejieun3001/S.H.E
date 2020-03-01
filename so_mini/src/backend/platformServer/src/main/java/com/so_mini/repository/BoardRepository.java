4b4a9ce6826da78780b432ab6847a1c5abeb0e5b
Leejieun3001 <jieun3001@gmail.com>
2020-03-01 17:57:02 +0900

    connect mysql with somini DB

--- so_mini/src/backend/platformServer/src/main/java/com/so_mini/repository/BoardRepository.java
+++ so_mini/src/backend/platformServer/src/main/java/com/so_mini/repository/BoardRepository.java
@@ -1,8 +1,6 @@
-package com.so_mini.repository;
-
-import com.so_mini.domain.Board;
-import com.so_mini.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BoardRepository extends JpaRepository<Board, Long> {
-}
+//package com.so_mini.repository;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface BoardRepository extends JpaRepository<Board, Long> {
+//}
