125b7c31d472e1f2970a4fadc0ee0e6ceb034abd
Leejieun3001 <jieun3001@gmail.com>
2020-03-03 13:56:11 +0900

    share database

--- so_mini/src/backend/platformServer/src/main/java/com/so_mini/domain/model/user.java
+++ so_mini/src/backend/platformServer/src/main/java/com/so_mini/domain/model/user.java
@@ -3,13 +3,15 @@ package com.so_mini.domain.model;
 import lombok.*;
 
 import javax.persistence.*;
+import java.io.Serializable;
 
 @Data
 @NoArgsConstructor
 @Getter
 @Table
 @Entity
-public class User {
+public class User implements Serializable {
+
     @Id
     @Column
     @GeneratedValue
