56353406cf1c069aad8b894bfc64da74182b3e57
Leejieun3001 <jieun3001@gmail.com>
2020-03-05 18:19:56 +0900

    clean project

--- so_mini/src/backend/authenticationServer/src/main/java/com/so_mini/repository/BoardRepository.java
+++
@@ -1,8 +0,0 @@
-package com.so_mini.repository;
-
-import com.so_mini.domain.Board;
-import com.so_mini.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BoardRepository extends JpaRepository<Board, Long> {
-}
--- so_mini/src/backend/authenticationServer/src/main/java/com/so_mini/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.so_mini.repository;
-
-import com.so_mini.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
